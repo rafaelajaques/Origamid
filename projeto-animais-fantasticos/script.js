454e33c1c8099d87ae70f4208cc5cbd93d6c3486
rafaelajaques <rafaelajaques@yahoo.com.br>
2022-11-23 18:26:09 -0300

    js

--- projeto-animais-fantasticos/script.js
+++ projeto-animais-fantasticos/script.js
@@ -1,16 +1,96 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-function activeTab(index){
-    tabContent.forEach((section)=>{
-        section.classList.remove('ativo')
-    })
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo')
 
-    tabContent[index].classList.add('ativo')
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo')
+            })
+
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index)
+            })
+        })
+    }
 }
+initTabNav()
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index)
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const activeClass = "ativo"
+
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeClass)
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion)
+        })
+    }
+}
+initAccordion()
+
+function initScrollSmoth() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+    function scrollToSection(event) {
+        event.prevenDefault()
+        const href = currentTarget.getAttribute('href')
+        const section = document.querySelector(href)
+
+        section.scrollIntoView({
+            behavior: 'smoth',
+            block: 'start'
+        })
+
+        // forma alternativa
+        // const topo = section.offsetTop
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth'
+        // })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection)
     })
-})
+}
+initScrollSmoth()
+
+function initAnimationScroll() {
+    const sections = document.querySelectorAll('.js-scroll')
+
+    if (sections.length) {
+        const windowHalf = innerHeight * 0.5
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top
+                const isSectionVisible = (sectionTop - windowHalf) < 0
+
+                if (isSectionVisible)
+                    section.classList.add('ativo')
+                else
+                    section.classList.remove('ativo')
+
+            })
+        }
+        animaScroll()
+
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+initAnimationScroll()
